b2de2746c0d42e4b7306b03814902901e9164417
Saleh Ashkboos <saleh.ashkboos@gmail.com>
2024-02-11 17:08:47 +0100

    .

--- baselines/gh_runs.bash
+++ baselines/gh_runs.bash
@@ -1,3 +1,3 @@
-python3 get_power.py > 1.out
-python3 get_power.py > 2.out
-python3 get_power.py > 3.out
+python3 get_power_nvml.py > 1.out
+python3 get_power_nvml.py > 2.out
+python3 get_power_nvml.py > 3.out
